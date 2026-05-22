17b4e40c0c1c292d21b0848a46846265d928c1ab
Nikolay <123203875+karpovru@users.noreply.github.com>
2026-05-23 01:12:16 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,49 @@
+async function loadClients() {
 
+  try {
+
+    const response = await fetch("/api/clients");
+
+    const clients = await response.json();
+
+    const table = document.getElementById("clientsTable");
+
+    table.innerHTML = "";
+
+    clients.forEach(client => {
+
+      table.innerHTML += `
+
+        <tr class="border-t">
+
+          <td class="p-4 font-medium">
+            ${client.name}
+          </td>
+
+          <td class="p-4">
+            ${client.phone}
+          </td>
+
+          <td class="p-4 text-2xl">
+            ${client.offer ? "✅" : "❌"}
+          </td>
+
+          <td class="p-4 text-2xl">
+            ${client.contract ? "✅" : "❌"}
+          </td>
+
+        </tr>
+
+      `;
+
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+
+}
+
+loadClients();
